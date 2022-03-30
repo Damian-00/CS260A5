5ec940ebdcaece7731e8ee443e54592114d9f1ec
David <m.david@digipen.edu>
2022-03-30 12:32:20 +0200

    Working on connection

--- src/networking/server.cpp
+++ src/networking/server.cpp
@@ -37,11 +37,13 @@ namespace CS260
 
 		SetSocketBlocking(mSocket, false);
 	}
+
 	Server::~Server()
 	{
 		closesocket(mSocket);
 		NetworkDestroy();
 	}
+
 	void Server::Tick()
 	{
 		// TOOD: We may start the game with less than 4 players
@@ -59,6 +61,16 @@ namespace CS260
 	{
 		PrintMessage("Accepting new client");
 
+		sockaddr senderAddress;
+		if(ReceiveSYN(senderAddress, packet))
+			if (SendSYNACK(senderAddress, packet))
+				if (ReceiveACKFromSYNACK(senderAddress, packet))
+				{ }
+
+	}
+
+	bool Server::ReceiveSYN(sockaddr& senderAddress, ConnectionPacket& packet)
+	{
 		WSAPOLLFD poll;
 		poll.fd = mSocket;
 		poll.events = POLLIN;
@@ -87,13 +99,13 @@ namespace CS260
 				else
 				{
 					// Make sure this is a SYN code
-					if (packet.GetCode() & SYNCODE)
+					if (packet.mCode & SYNCODE)
 					{
 						PrintMessage("Received SYN code", mVerbose);
 						return true;
 					}
 					// We were told to reset connection establishment
-					else if (packet.GetCode() & RSTCODE)
+					else if (packet.mCode & RSTCODE)
 					{
 						PrintMessage("Reseting connection.", mVerbose);
 						return false;
@@ -111,7 +123,136 @@ namespace CS260
 			}
 		}
 		return false;
+	}
 
+	bool Server::SendSYNACK(sockaddr& senderAddress, ConnectionPacket& packet)
+	{
+		const int size = sizeof(sockaddr);
+
+		// We will only send the SYNACKCODE and the ACK number
+		// Which are included by themselsves, so we do not add anything
+		packet.AttachACK(0);
+
+		// Change the type of message
+		packet.SetCode(SYNACKCODE);
+
+		// We only need to send the header as it will contatin the code and the required ack
+		int bytesSent = ::sendto(mSocket, reinterpret_cast<char*>(&packet), sizeof(ProtocolHeader), 0, &senderAddress, size);
+
+		// Handle errors
+		if (bytesSent == SOCKET_ERROR)
+		{
+			PrintError("Sending SYN ACK accepting new client");
+			return false;
+		}
+		else
+		{
+			PrintMessage("Sending SYNACK with sequence " + std::to_string(packet.GetSequence()), mVerbose);
+			return true;
+		}
+	}
+
+	bool Server::ReceiveACKFromSYNACK(sockaddr& senderAddress, ConnectionPacket& packet)
+	{
+		unsigned expectedACK = packet.GetExpectACK();
+		auto clock = now();
+		socklen_t size = sizeof(senderAddress);
+		do
+		{
+			WSAPOLLFD poll;
+			poll.fd = mSocket;
+			poll.events = POLLIN;
+			if (WSAPoll(&poll, 1, timeout) > 0)
+			{
+				if (poll.revents & POLLERR)
+				{
+					PrintError("Polling receive ACK from SYNACK");
+					return false;
+				}
+				else
+				{
+					PrintMessage("Waiting for ACK to SYNACK expected ACK " + std::to_string(expectedACK), mVerbose); ;
+					// Receive ACK of SYNACK
+					int bytesReceived = ::recvfrom(mSocket, reinterpret_cast<char*>(&packet), sizeof(ProtocolPacket), 0, &senderAddress, &size);
+					if (bytesReceived == SOCKET_ERROR)
+					{
+						PrintError("Receiving ACK from SYNACK");
+						return false;
+					}
+					// Make sure it is an ACK code
+					if (packet.GetCode() & ACKCODE)
+					{
+						unsigned packetACK = packet.GetACK();
+						PrintMessage("Received ACK to SYNACK " + std::to_string(packetACK), mVerbose); ;
+
+						// Make sure the current ack is the expected one
+						if (packetACK == expectedACK)
+						{
+							// Connect to the client socket to the given address to allow calling
+							// send and recv with the client being the default receiver and sender
+							// We will forget about new clients sending messages while we are transferring a file
+							if (::connect(mSocket, &senderAddress, sizeof(senderAddress)) == SOCKET_ERROR)
+							{
+								PrintError("on ::connect to client socket");
+								return false;
+							}
+							else
+							{
+								PrintMessage("[SERVER] Connected socket correctly.", mVerbose);
+
+								unsigned msgSize = packet.GetMsgSize();
+
+								// Make the filename string big enough
+								mCurrentFilename.resize(msgSize);
+
+								// Copy the attached filename
+								::memcpy(mCurrentFilename.data(), packet.msg.data(), msgSize);
+								return true;
+							}
+						}
+					}
+					// RESET
+					else if (packet.GetCode() & RSTCODE)
+					{
+						PrintMessage("Received RST", mVerbose);
+						return false;
+					}
+					// We received any other type of message probably mixed from a previous client
+					else
+					{
+						PrintMessage("Received unknown type of message", mVerbose);
+						// Although this message is most likely to be ignored, send a reset request in case
+						// two clients are trying to connect at the same time
+						SendRST(senderAddress, packet);
+					}
+				}
+			}
+			// Timeout of 5 seconds
+		} while (ms_since(clock) < 5000);
+
+		// In case of timeout waiting for the ACK from SYNACK restart connection
+		PrintMessage("Timeout waiting for ACK from SYNACK", mVerbose);
+		SendRST(senderAddress, packet);
+		return false;
+	}
+
+	/*	\fn SendRST
+	\brief	Sends reset message
+	*/
+	bool Server::SendRST(sockaddr& senderAddress, ConnectionPacket& packet)
+	{
+		packet.AttachACK(0);
+		packet.SetCode(RSTCODE);
+		int size = sizeof(senderAddress);
+		int bytesReceived = ::sendto(mSocket, reinterpret_cast<char*>(&packet), sizeof(packet), 0, &senderAddress, size);
+		if (bytesReceived == SOCKET_ERROR)
+		{
+			PrintError("Error sending RST");
+		}
+		else
+			PrintMessage("[SERVER] Sending RST correctly", mVerbose);
+
+		return false;
 	}
 
 	void Server::PrintMessage(const std::string& msg)
--- src/networking/server.hpp
+++ src/networking/server.hpp
@@ -5,18 +5,23 @@
 
 namespace CS260
 {
-	struct ShipPacket
-	{
-		vec2 pos;
-		vec2 vel;
-		float score;
-	};
+
+	const unsigned char SYNCODE = 0b1;
+	const unsigned char SYNACKCODE = 0b10;
+	const unsigned char ACKCODE = 0b1000;
+	const unsigned char RSTCODE = 0b1000000;
 
 	struct ClientInfo
 	{
 		unsigned char mID;
 		sockaddr mEndpoint;
 	};
+	struct ConnectionPacket
+	{
+		unsigned mACK;
+		unsigned char mCode;		
+	};
+
 	class Server
 	{
 		bool mVerbose;
@@ -40,6 +45,29 @@ namespace CS260
 	private:
 
 		void HandleNewClients();
+
+		/*	\fn ReceiveSYN
+		\brief	Receive SYN message
+		*/
+		bool ReceiveSYN(sockaddr& senderAddress, ConnectionPacket& packet);
+
+		/*	\fn SendSYNACK
+		\brief	Sends SYNACK message
+		*/
+		bool SendSYNACK(sockaddr& senderAddress, ConnectionPacket& packet);
+
+		/*	\fn ReceiveACKFromSYNACK
+		\brief	Receive ACK from SYNACK
+		*/
+		bool ReceiveACKFromSYNACK(sockaddr& senderAddress, ConnectionPacket& packet);
+
+		/*	\fn SendRST
+		\brief	Sends reset message
+		*/
+		bool SendRST(sockaddr& senderAddress, ConnectionPacket& packet);
+
+
+
 		void PrintMessage(const std::string& msg);
 		void PrintError(const std::string& msg);
 	};
